e1b7bbf81c8e7c260800254d8134446ef89388a9
Josh Cash <joshcash86@gmail.com>
2023-05-16 19:14:58 -0400

    added js class selectors

---
+++ project3/script.js
@@ -0,0 +1,20 @@
+const cardNumber = document.querySelector('.card-number');
+const cardName = document.querySelector('.card-name');
+const cardDate = document.querySelector('.card-date');
+
+const inputName = document.querySelector('.input-name');
+const inputNumber = document.querySelector('.input-number');
+const inputMonth = document.querySelector('.input-month');
+const inputYear = document.querySelector('.input-year');
+const inputCvc = document.querySelector('.input-cvc');
+
+
+
+
+
+
+
+
+
+
+
