c4b6311f9df1cae6132584c69a58dba4580bffa6
avrubio <ariadna.vanegas@gmail.com>
2023-05-17 14:45:48 -0400

    Form Validation script started

--- project3/script.js
+++ project3/script.js
@@ -19,10 +19,61 @@ inputYear.addEventListener("input", updateCard);
 inputCvc.addEventListener("input", updateCard);
 
 function updateCard() {
-  cardNumber.innerHTML = inputNumber.value === "" ? "0000 0000 0000 0000" : inputNumber.value.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
-  cardName.innerHTML = inputName.value === "" ? "JANE APPLESEED" : inputName.value.toUpperCase();
-  cardDate.innerHTML = inputMonth.value === "" || inputYear.value === "" ? "00/00" : inputMonth.value + "/" + inputYear.value;
+  cardNumber.innerHTML =
+    inputNumber.value === ""
+      ? "0000 0000 0000 0000"
+      : inputNumber.value.replace(/\W/gi, "").replace(/(.{4})/g, "$1 ");
+  cardName.innerHTML =
+    inputName.value === "" ? "JANE APPLESEED" : inputName.value.toUpperCase();
+  cardDate.innerHTML =
+    inputMonth.value === "" || inputYear.value === ""
+      ? "00/00"
+      : inputMonth.value + "/" + inputYear.value;
   cardCvc.innerHTML = inputCvc.value === "" ? "000" : inputCvc.value;
 }
 
+// form validation
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+});
 
+const setErrot = (element, message) => {
+  const inputControl = element.ParentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const validateInputs = () => {
+  const cardNumberValue = cardNumber.value.trim();
+  const cardNameValue = cardName.value.trim();
+  const cardDateValue = cardDate.value.trim();
+  const cardCvcValue = cardCvc.value.trim();
+
+  if (cardNumberValue === "") {
+    setError(cardNumber, "Numbers required");
+  } else {
+    setSuccess(cardNumber);
+  }
+
+  if (cardNameValue === "") {
+    setError(cardName, "Name on card required");
+  } else {
+    setSuccess(cardName);
+  }
+
+  if (cardDateValue === "") {
+    setError(cardDate, "Date is required");
+  } else {
+    setSuccess(cardDate);
+  }
+
+  if (cardCvcValue === "") {
+    setError(cardCvc, "Cvc is required");
+  } else {
+    setSuccess(cardCvc);
+  }
+};
